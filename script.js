5264c69654c3bd8b7921fad89965051126eeb42d
jinyeol <jinyeol.dev@gmail.com>
2026-07-30 14:41:16 +0900

    Deploy JY website

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll('a[href^="#"]').forEach((link) => {
+    link.addEventListener("click", () => {
+      const target = document.querySelector(link.getAttribute("href"));
+      if (target) target.setAttribute("tabindex", "-1");
+    });
+  });
+});
